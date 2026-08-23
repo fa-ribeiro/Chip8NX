731a0811b33d96e9d980cad723c5beb9cd417371
Fernando Ribeiro <fribeiro@fribeiro.org>
2026-08-23 18:54:43 +0100

    Improve RandomNumberGenerator tests coverage

--- src/random/test-random-number-generator.test.ts
+++ src/random/test-random-number-generator.test.ts
@@ -2,6 +2,31 @@ import { assertEquals } from "@std/assert";
 import { byte } from "../core/types/byte.ts";
 import { TestRandomNumberGenerator } from "./test-random-number-generator.ts";
 
+Deno.test("TestRandomNumberGenerator constructor accepts an array of bytes", () => {
+  const random = new TestRandomNumberGenerator([
+    byte(0x12),
+    byte(0xab),
+    byte(0x42),
+  ]);
+
+  assertEquals(random.nextByte(), byte(0x12));
+  assertEquals(random.nextByte(), byte(0xab));
+  assertEquals(random.nextByte(), byte(0x42));
+});
+
+Deno.test("TestRandomNumberGenerator constructor rejects an empty array", () => {
+  try {
+    new TestRandomNumberGenerator([]);
+    throw new Error("Expected constructor to throw an error");
+  } catch (error) {
+    if (!(error instanceof RangeError)) {
+      throw new Error(
+        `Expected RangeError, but got ${error.constructor.name}`,
+      );
+    }
+  }
+});
+
 Deno.test("TestRandomNumberGenerator returns values in order", () => {
   const random = new TestRandomNumberGenerator([
     byte(0x12),
