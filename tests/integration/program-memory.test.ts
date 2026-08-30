19d1098670177e2438f64cf7fbe2415164231701
Fernando Ribeiro <fribeiro@fribeiro.org>
2026-08-30 19:38:11 +0100

    Add program load into memory integration tests

---
+++ tests/integration/program-memory.test.ts
@@ -0,0 +1,67 @@
+import { assertEquals } from "@std/assert";
+
+import { address } from "../../src/core/types/address.ts";
+import { byte } from "../../src/core/types/byte.ts";
+import { DisplayBuffer } from "../../src/display/display-buffer.ts";
+import { ClassicFont } from "../../src/font/classic-font.ts";
+import { Decoder } from "../../src/instruction/decoder.ts";
+import { TestKeyboard } from "../../src/keyboard/test-keyboard.ts";
+import { MemoryImage } from "../../src/memory/memory-image.ts";
+import { MemoryImageLoader } from "../../src/memory/memory-image-loader.ts";
+import { Ram } from "../../src/memory/ram.ts";
+import { TestRandomNumberGenerator } from "../../src/random/test-random-number-generator.ts";
+import { Timer } from "../../src/timer/timer.ts";
+import { Cpu } from "../../src/cpu/cpu.ts";
+import type { ExecutionContext } from "../../src/cpu/execution-context.ts";
+import { IndexRegister } from "../../src/cpu/index-register/index-register.ts";
+import { InstructionExecutor } from "../../src/cpu/instruction-executor.ts";
+import { ProgramCounter } from "../../src/cpu/program-counter/program-counter.ts";
+import { registerIndex } from "../../src/cpu/registers/register-index.ts";
+import { Registers } from "../../src/cpu/registers/registers.ts";
+import { Stack } from "../../src/cpu/stack/stack.ts";
+
+Deno.test("CPU executes a program loaded into memory from a MemoryImage", () => {
+  const programStartAddress = address(0x200);
+
+  const program = new MemoryImage([
+    0x60,
+    0x42, // LD V0, 0x42
+    0x70,
+    0x01, // ADD V0, 0x01
+  ]);
+
+  const memory = new Ram(0x1000);
+  const registers = new Registers();
+
+  const loader = new MemoryImageLoader();
+
+  loader.load(memory, programStartAddress, program);
+
+  const context: ExecutionContext = {
+    registers,
+    memory,
+    stack: new Stack(),
+    programCounter: new ProgramCounter(programStartAddress),
+    indexRegister: new IndexRegister(),
+    soundTimer: new Timer(),
+    delayTimer: new Timer(),
+    displayBuffer: new DisplayBuffer(64, 32),
+    keyboard: new TestKeyboard(),
+    font: new ClassicFont(),
+    randomNumberGenerator: new TestRandomNumberGenerator([byte(0)]),
+  };
+
+  const cpu = new Cpu(context, new Decoder(), new InstructionExecutor());
+
+  cpu.step();
+
+  assertEquals(registers.get(registerIndex(0x0)), byte(0x42));
+
+  assertEquals(context.programCounter.getValue(), address(0x202));
+
+  cpu.step();
+
+  assertEquals(registers.get(registerIndex(0x0)), byte(0x43));
+
+  assertEquals(context.programCounter.getValue(), address(0x204));
+});
